2b51e6dc8bc1d70f2694f6f1bab49ec995e69146
Harish Marella <mharish@pandorarndlabs.com>
2024-08-14 17:34:20 +0530

    first commit

---
+++ src/navigation/NavigationContainer/AppNavigation.tsx
@@ -0,0 +1,14 @@
+// src/navigation/NavigationContainer.tsx
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+const AppNavigation: React.FC = () => {
+    return (
+        <NavigationContainer>
+            <>
+            </>
+        </NavigationContainer>
+    );
+};
+
+export default AppNavigation;
